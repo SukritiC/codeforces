1c3de9a96693716be4869cd3c6425636ec14ee1e
sukritichatterjee <sukriti0892@gmail.com>
2025-11-02 16:15:56 +0530

    DP Question -  tabulation - cherry picking tabulation

--- DP/2D/cherry-picking-ii.py
+++ DP/2D/cherry-picking-ii.py
@@ -39,3 +39,56 @@ class Solution:
         m = len(matrix[0])
         dp = [[[-1 for _ in range(m)] for _ in range(m)] for _ in range(n)]
         return self.func(0, 0, m - 1, n, m, matrix, dp)
+
+# Tabulation
+
+class Solution:
+
+
+    def cherryPickup(self, matrix):
+        n = len(matrix)
+        m = len(matrix[0])
+        dp = [[[-1 for _ in range(m)] for _ in range(m)] for _ in range(n)]
+
+        # base case - adding value for j1 --> 0 to m-1 & j2 --> 0 to m-1
+
+        for j1 in range(m):
+            for j2 in range(m):
+                if j1 == j2:
+                    dp[n - 1][j1][j2] = matrix[n - 1][j1]
+                else:
+                    dp[n - 1][j1][j2] = matrix[n - 1][j1] + matrix[n - 1][j2]
+
+        for i in range(n - 2, -1, -1):
+            for j1 in range(m):
+                for j2 in range(m):
+                    max_value = int(-1e9)
+                    for dj1 in range(-1, 2):
+                        for dj2 in range(-1, 2):
+                            ans = 0
+                            if j1 == j2:
+                                ans = matrix[i][j1]
+                            else:
+                                ans = matrix[i][j1] + matrix[i][j2]
+
+                            if (j1 + dj1 < 0 or j1 + dj1 >= m) or (j2 + dj2 < 0 or j2 + dj2 >= m):
+                                ans += -1e9
+                            else:
+                                ans += + dp[i + 1][j1 + dj1][j2 + dj2]
+                            max_value = max(ans, max_value)
+                    dp[i][j1][j2] = max_value
+
+        return dp[0][0][m - 1]
+
+
+
+
+
+
+
+
+
+
+
+
+
